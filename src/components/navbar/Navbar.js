fde2f0fb065695c90badb84df3fbc9813538bc0f
Raghani Madhu <raghanimadhu@gmail.com>
2023-05-22 17:42:02 +0530

    completed Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,36 +4,44 @@ import {
   AiOutlineHeart,
   AiOutlineUser,
   AiOutlineShoppingCart,
+  // AiOutlineSearch,
 } from "react-icons/ai";
 import "../navbar/Navbar.css";
 
 function Navbar() {
   return (
-    <>
-      <nav className="navigation container">
-        <div className="nav-brand">
-          <Link className="link" to="/">
+    <div className="nav-bar">
+      <nav className="nav-full">
+        <div className="nav-left">
+          <Link className="link app-name" to="/">
             NightAngels
           </Link>
         </div>
-
-        <div className="nav-explore">
-          <input type="text" placeholder="Search" />
+        <div className="nav-center">
+          <label>
+            <input
+              type="text"
+              placeholder="Search"
+              className="nav-search-input"
+            />
+          </label>
+        </div>
+        <div className="nav-right">
           <Link className="link" to="/productlists">
             Explore
           </Link>
           <Link className="link" to="/">
             <AiOutlineUser size={28} />
           </Link>
-          <Link className="link" href="/wishlist">
+          <Link className="link" to="/wishlist">
             <AiOutlineHeart size={28} />
           </Link>
-          <Link className="link" href="/cart">
+          <Link className="link" to="/cart">
             <AiOutlineShoppingCart size={28} />
           </Link>
         </div>
       </nav>
-    </>
+    </div>
   );
 }
 
