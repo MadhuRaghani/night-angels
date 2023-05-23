f95dfe45491a22c506a35124c84a20439dfcdf2f
Raghani Madhu <raghanimadhu@gmail.com>
2023-05-24 00:40:02 +0530

    Home Page Functionality

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,9 +2,11 @@ import React, { useContext } from "react";
 import "../home/Home.css";
 import CategoryCard from "./CategoryCard";
 import { ProductsContext } from "../../contexts/ProductsContext";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const { categories } = useContext(ProductsContext);
+  const { categories, filtersDispatch } = useContext(ProductsContext);
+  const navigate = useNavigate();
 
   return (
     <div className="home">
@@ -13,12 +15,25 @@ function Home() {
           className="hero-img"
           src="https://julietindia.com/wp-content/uploads/2023/01/3.webp"
           alt="hero"
+          onClick={() => {
+            navigate("/productlists");
+          }}
         />
       </header>
       <h3>Featured Categories</h3>
       <div className="all-categories">
         {categories.map((category) => (
-          <CategoryCard key={category._id} categoryDetails={category} />
+          <div
+            onClick={() => {
+              filtersDispatch({
+                type: "CATEGORIES",
+                payload: category.categoryName,
+              });
+              navigate("/productlists");
+            }}
+          >
+            <CategoryCard key={category._id} categoryDetails={category} />
+          </div>
         ))}
       </div>
     </div>
